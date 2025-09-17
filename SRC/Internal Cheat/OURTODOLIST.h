41cb798c1ef6fb78ce5f9a19742900c805bddfa7
Kasowkz <workingspace20@gmail.com>
2025-09-17 22:17:11 +0200

    Upload Files

---
+++ SRC/Internal Cheat/OURTODOLIST.h
@@ -0,0 +1,10 @@
+
+/* 
+* Clean up the code in GameSDK.h - johnn
+* Fix a few issues within the Visuals - jonn ( shouldn't be to major )
+* Implement MinHook ( For Some Cool Features )
+* Add Aimbot
+* Add Silent Aim
+* Fix Skeleton ESP with the lines ( buggy )
+* 
+*/
